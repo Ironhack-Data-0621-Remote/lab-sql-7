bf89ad0e51ae3a5ddee678288466e3a42ede6b32
unknown <tom.dubun@parrot.com>
2021-06-27 15:11:20 +0200

    Solutions for Lab 7 SQL Tom Dubun

--- solutions.sql
+++ solutions.sql
@@ -2,22 +2,45 @@
 -- you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. 
 -- So we do not want to include this last name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", 
 -- hence we would want this in our output list.
-
+use sakila;
+select last_name, count(last_name)
+from actor
+group by last_name
+having count(last_name) = 1;
 
 -- 2. Which last names appear more than once? We would use the same logic as in the previous question but this time we want to include the last names of the actors
 -- where the last name was present more than once.
-
+use sakila;
+select last_name, count(last_name)
+from actor
+group by last_name
+having count(last_name) > 1;
 
 -- 3. Using the rental table, find out how many rentals were processed by each employee.
-
+select staff_id, count(rental_id) as nb_rentals
+from rental
+group by staff_id;
 
 -- 4. Using the film table, find out how many films were released each year.
-
+select release_year, count(film_id) as films_released
+from film
+group by release_year;
 
 -- 5. Using the film table, find out for each rating how many films were there.
+select rating, count(film_id) as films_released
+from film
+group by rating;
 
 
 -- 6. What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
+select rating, round(avg(length),2) as mean_length
+from film
+group by rating;
 
 
 -- 7. Which kind of movies (rating) have a mean duration of more than two hours?
+select rating, round(avg(length),2) as mean_length
+from film
+group by rating
+having mean_length > 120;
+
