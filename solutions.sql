72dbe8c292e790fc6e27fde43a17c13c84c3bdc7
sumampouw <s.sumampouw@hotmail.com>
2021-06-28 20:50:20 +0200

    lab 7 spica

--- solutions.sql
+++ solutions.sql
@@ -2,22 +2,48 @@
 -- you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. 
 -- So we do not want to include this last name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", 
 -- hence we would want this in our output list.
-
+SELECT *
+FROM actor
+WHERE last_name IN 
+	(SELECT last_name 
+	FROM actor 
+	GROUP BY last_name
+	HAVING COUNT(last_name) = 1)
+ORDER BY last_name;
 
 -- 2. Which last names appear more than once? We would use the same logic as in the previous question but this time we want to include the last names of the actors
 -- where the last name was present more than once.
-
+SELECT *
+FROM actor
+WHERE last_name IN 
+	(SELECT last_name 
+	FROM actor 
+	GROUP BY last_name
+	HAVING COUNT(last_name) > 1)
+ORDER BY last_name;
 
 -- 3. Using the rental table, find out how many rentals were processed by each employee.
-
+SELECT COUNT(rental_date), staff_id
+FROM sakila.rental
+GROUP BY staff_id;
 
 -- 4. Using the film table, find out how many films were released each year.
-
+SELECT COUNT(release_year), rating
+FROM sakila.film
+GROUP BY rating;
 
 -- 5. Using the film table, find out for each rating how many films were there.
-
+SELECT COUNT(film_id), rating
+FROM sakila.film
+GROUP BY rating;
 
 -- 6. What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
-
+SELECT rating, ROUND(AVG(length),2)
+FROM film
+GROUP BY rating;
 
 -- 7. Which kind of movies (rating) have a mean duration of more than two hours?
+SELECT rating, ROUND(AVG(length), 2) c
+FROM sakila.film
+GROUP BY rating
+HAVING c > 120;
