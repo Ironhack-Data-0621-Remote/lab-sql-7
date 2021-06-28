3a38c43c7ed44dbd4a72aaa7a000062adf737689
Elissadejong <elissa@mepps.nl>
2021-06-28 13:17:14 +0200

    lab_sql_7_finished

--- solutions.sql
+++ solutions.sql
@@ -1,23 +1,38 @@
--- 1. In the table actor, which are the actors whose last names are not repeated? For example if you would sort the data in the table actor by last_name, 
--- you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. 
--- So we do not want to include this last name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", 
--- hence we would want this in our output list.
-
-
--- 2. Which last names appear more than once? We would use the same logic as in the previous question but this time we want to include the last names of the actors
--- where the last name was present more than once.
-
-
--- 3. Using the rental table, find out how many rentals were processed by each employee.
-
-
--- 4. Using the film table, find out how many films were released each year.
-
-
--- 5. Using the film table, find out for each rating how many films were there.
-
-
--- 6. What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
-
-
--- 7. Which kind of movies (rating) have a mean duration of more than two hours?
+USE sakila;
+-- In the table actor, which are the actors whose last names are not repeated?
+SELECT last_name,
+RANK() OVER(ORDER BY last_name)
+FROM actor; 
+-- Is there any way a rank function would work to obtain the answer?
+-- AKA a way to combine a (dense/)rank function with count? 
+-- Just to experiment hahah :))
+SELECT last_name, COUNT(*) 
+FROM actor
+GROUP BY last_name
+HAVING COUNT(*) = 1;
+-- Which last names appear more than once? 
+SELECT last_name, COUNT(*)
+FROM actor
+GROUP BY last_name
+HAVING COUNT(*) > 1;
+-- Using the rental table, find out how many rentals were processed by each employee.
+SELECT staff_id, COUNT(*)
+FROM rental
+GROUP BY staff_id;
+-- Using the film table, find out how many films were released each year.
+SELECT release_year, COUNT(*)
+FROM film
+GROUP BY release_year;
+-- Using the film table, find out for each rating how many films were there.
+SELECT rating, COUNT(*)
+FROM film
+GROUP BY rating;
+-- What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
+SELECT rating, ROUND(AVG(length), 2) as average_length
+FROM film
+GROUP BY rating;
+-- Which kind of movies (rating) have a mean duration of more than two hours?
+SELECT rating, ROUND(AVG(length), 2) as average_length
+FROM film
+GROUP BY rating
+HAVING ROUND(AVG(length), 2) > 120;
