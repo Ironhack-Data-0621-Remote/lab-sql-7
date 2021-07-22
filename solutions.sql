e1bece40818fa1024b8d9ded63aa5f250847087f
Ziga <ziga.jarh.kolar@gmail.com>
2021-07-22 14:01:23 +0200

    ZKsql7

--- solutions.sql
+++ solutions.sql
@@ -1,23 +1,46 @@
--- 1. In the table actor, which are the actors whose last names are not repeated? For example if you would sort the data in the table actor by last_name, 
--- you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. 
--- So we do not want to include this last name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", 
--- hence we would want this in our output list.
+-- lab-sql-7
+USE sakila;
+-- In the table actor, which are the actors whose last names are not repeated?
 
+SELECT DISTINCT last_name
+FROM actor
+GROUP BY first_name, last_name;
 
--- 2. Which last names appear more than once? We would use the same logic as in the previous question but this time we want to include the last names of the actors
--- where the last name was present more than once.
+-- Which last names appear more than once?
 
+SELECT last_name
+FROM actor
+GROUP BY last_name
+HAVING COUNT(last_name) > 1;
 
--- 3. Using the rental table, find out how many rentals were processed by each employee.
+-- Using the rental table, find out how many rentals were processed by each employee.
 
+SELECT COUNT(rental_id) as rentals_processed, staff_id
+FROM rental
+GROUP BY staff_id;
 
--- 4. Using the film table, find out how many films were released each year.
+-- Using the film table, find out how many films were released each year.
 
+SELECT COUNT(film_id), release_year
+FROM film
+GROUP BY release_year;
 
--- 5. Using the film table, find out for each rating how many films were there.
+-- Using the film table, find out for each rating how many films were there.
 
+SELECT COUNT(film_id), rating
+FROM film
+GROUP BY rating;
 
--- 6. What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
+-- What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
 
+SELECT ROUND(AVG(length),2) as "avg_lenght", rating
+from film
+GROUP BY rating;
 
--- 7. Which kind of movies (rating) have a mean duration of more than two hours?
+-- Which kind of movies (rating) have a mean duration of more than two hours?
+
+SELECT ROUND(AVG(length),2) as "avg_lenght", rating
+from film
+WHERE length > 120
+GROUP BY rating;
+
